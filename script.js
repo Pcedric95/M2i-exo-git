d073b83ce094bc3a320db4e0326c796581f8b025
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-16 14:10:24 +0200

    Factorisation des fonctions de calcul

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
 // Calculette flexbox fonctionnelle - HTML, CSS et JavaScript
 
-const ecran = document.querySelector('.ecran'); // Sélectionne l'élément avec la classe 'ecran'
-const boutons = document.querySelectorAll('.touches div'); // Sélectionne tous les divs à l'intérieur de l'élément avec la classe 'touches'
+//Fonstions
+
+function reinitialiser() {
+  ecran.textContent = '0';
+}
+
+function inverserLeSigne(valeur) {
+  if (valeur === "+/_") {
+    let nombre = parseFloat(ecran.textContent);
+    nombre = -nombre;
+    ecran.textContent = nombre;
+    return;
+  }
+}
+
+function pourcentagevaleur() {
+  if (valeur === "%") {
+    let nombre = parseFloat(ecran.textContent);
+    nombre = nombre / 100;
+    ecran.textContent = nombre;
+    return;
+  }
+}
+
+function calculDuResulat(valeur) {
+  if (valeur === "=") {
+    ecran.textContent = eval(ecran.textContent); // afficher le résultat
+  } else if (ecran.textContent === "0") {
+    ecran.textContent = valeur;
+  } else {
+    ecran.textContent += valeur;
+  }
+}
+
+const ecran = document.querySelector(".ecran"); // Sélectionne l'élément avec la classe 'ecran'
+const boutons = document.querySelectorAll(".touches div"); // Sélectionne tous les divs à l'intérieur de l'élément avec la classe 'touches'
 
 // Fonction pour ajouter un chiffre ou un opérateur à l'affichage
-boutons.forEach((boutons) => 
-    {
-        boutons.addEventListener('click', (e) => {
-            let valeur = e.target.textContent; // Récupère le texte du bouton cliqué 
-            if (valeur === 'C'){
-                ecran.textContent = '0';  
-                return
-              }
+boutons.forEach((boutons) => {
+  boutons.addEventListener("click", (e) => {
+    let valeur = e.target.textContent; // Récupère le texte du bouton cliqué
+    if (valeur === "C") return reinitialiser(valeur);
+    if (valeur === "+/_") return inverserLeSigne(valeur);
+    if (valeur === "%") return pourcentage(valeur);
+    if (valeur === "=") return calculDuResulat(valeur);
+    // ajouter la valeur à l’écran
+    return ajouterValeur(valeur);
 
-            if (valeur === '+/_'){
-                let nombre = parseFloat(ecran.textContent);
-                nombre = -nombre;
-                ecran.textContent = nombre;
-                return
-              }
-            if (valeur === '%'){
-                let nombre = parseFloat(ecran.textContent);
-                nombre = nombre / 100;
-                ecran.textContent = nombre;
-                return;
-            }
-            if (valeur === '=') {
-                ecran.textContent = eval(ecran.textContent); // afficher le résultat
-              } else if (ecran.textContent === '0') {
-                ecran.textContent = valeur;
-              } else {
-                ecran.textContent += valeur;
-              }
-        console.log(valeur); // J'affiche la valeur clicqué dans la console
-       
-        
-
-    })        
-})    
-
+    console.log(valeur); // J'affiche la valeur clicqué dans la console
+  });
+});
