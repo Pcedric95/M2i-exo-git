b3c0e91a6a511175529d199e8c8957e4cd92a903
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-16 14:26:07 +0200

    Factorisation des fonctions de calcul, modification de la boucle principale

--- script.js
+++ script.js
@@ -1,53 +1,59 @@
 // Calculette flexbox fonctionnelle - HTML, CSS et JavaScript
 
-//Fonstions
+//------------------- Fonctions --------------------------
 
+// Réinitialiser l'écran à zéro
 function reinitialiser() {
   ecran.textContent = '0';
 }
 
-function inverserLeSigne(valeur) {
-  if (valeur === "+/_") {
-    let nombre = parseFloat(ecran.textContent);
-    nombre = -nombre;
-    ecran.textContent = nombre;
-    return;
-  }
+// Inverser le signe du nombre affiché
+function inverserLeSigne() {
+  let nombre = parseFloat(ecran.textContent);
+  nombre = -nombre;
+  ecran.textContent = nombre;
 }
 
-function pourcentagevaleur() {
-  if (valeur === "%") {
-    let nombre = parseFloat(ecran.textContent);
-    nombre = nombre / 100;
-    ecran.textContent = nombre;
-    return;
-  }
+
+// Applique un pourcentage à la valeur
+function pourcentage() {
+  let nombre = parseFloat(ecran.textContent);
+  nombre = nombre / 100;
+  ecran.textContent = nombre;
 }
 
-function calculDuResulat(valeur) {
-  if (valeur === "=") {
-    ecran.textContent = eval(ecran.textContent); // afficher le résultat
-  } else if (ecran.textContent === "0") {
+// Calcul le résulat
+function calculDuResultat() {
+  ecran.textContent = eval(ecran.textContent);
+}
+
+
+// Ajoute un chiffre ou un opérateur à l’affichage
+function ajouterValeur(valeur) {
+  if (ecran.textContent === '0') {
     ecran.textContent = valeur;
   } else {
     ecran.textContent += valeur;
   }
 }
 
+
+// Sélectionner les éléments du DOM
 const ecran = document.querySelector(".ecran"); // Sélectionne l'élément avec la classe 'ecran'
 const boutons = document.querySelectorAll(".touches div"); // Sélectionne tous les divs à l'intérieur de l'élément avec la classe 'touches'
 
-// Fonction pour ajouter un chiffre ou un opérateur à l'affichage
+// Boucle pour chaque boutons
 boutons.forEach((boutons) => {
   boutons.addEventListener("click", (e) => {
     let valeur = e.target.textContent; // Récupère le texte du bouton cliqué
-    if (valeur === "C") return reinitialiser(valeur);
-    if (valeur === "+/_") return inverserLeSigne(valeur);
-    if (valeur === "%") return pourcentage(valeur);
-    if (valeur === "=") return calculDuResulat(valeur);
+    console.log(valeur); // affiche la valeur clicqué dans la console
+    if (valeur === "C") return reinitialiser();
+    if (valeur === "+/_") return inverserLeSigne();
+    if (valeur === "%") return pourcentage();
+    if (valeur === "=") return calculDuResultat();
+
     // ajouter la valeur à l’écran
     return ajouterValeur(valeur);
 
-    console.log(valeur); // J'affiche la valeur clicqué dans la console
   });
 });
