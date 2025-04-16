052d8f7d5eb2892074ea5fe0f4bf38f6425a7eef
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-16 12:26:10 +0200

    Ajout du dossier dexercice

---
+++ script.js
@@ -0,0 +1,28 @@
+// Calculette flexbox fonctionnelle - HTML, CSS et JavaScript
+
+const ecran = document.querySelector('.ecran'); // Sélectionne l'élément avec la classe 'ecran'
+const boutons = document.querySelectorAll('.touches div'); // Sélectionne tous les divs à l'intérieur de l'élément avec la classe 'touches'
+
+// Fonction pour ajouter un chiffre ou un opérateur à l'affichage
+boutons.forEach((boutons) => 
+    {
+        boutons.addEventListener('click', (e) => {
+            const valeur = e.target.textContent; // Récupère le texte du bouton cliqué 
+            if (valeur === 'C'){
+                ecran.textContent = '0';  
+                return
+              }
+            if (valeur === '=') {
+                ecran.textContent = eval(ecran.textContent); // afficher le résultat
+              } else if (ecran.textContent === '0') {
+                ecran.textContent = valeur;
+              } else {
+                ecran.textContent += valeur;
+              } 
+        console.log(valeur); // J'affiche la valeur clicqué dans la console
+       
+        
+
+    })        
+})    
+
