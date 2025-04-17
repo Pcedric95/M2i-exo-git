778cac57e7e03292fc0c428c6159c28f50f916df
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-17 10:35:13 +0200

    vérification si nom valide

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const form = document.querySelector('form-container')
-const nomInput = document.querySelector('nom')
-const dateInput = document.querySelector('date')
-const heureInput = document.querySelector('time')
-const participantsInput = document.querySelector('nbParticipants')
-const bouton = document.querySelector('button')
+//Sélection des éléments
+
+const form = document.querySelector('form-container');
+const nomInput = document.querySelector('#nom');
+const dateInput = document.querySelector('#date');
+const heureInput = document.querySelector('#time');
+const participantsInput = document.querySelector('#nbParticipants');
+const bouton = document.querySelector('button');
+
+// stockage des rendez-vous
+
+const reservations = [];
+
+// empêcher le rechargement de la page à chaque clic
+bouton.addEventListener('click', (e) =>
+{
+    e.preventDefault(); // empêcher le rechargement de la page
+    console.clear(); // supprimer la console à chaque clic
+})
+
+
+const nom = nomInput.value;
+if (nom.length < 3) {
+    alert("⚠️ Attention le nom doit contenir au moins 3 lettres.");
+    return;
+}
+
+// Vérifie que le nom ne contient que des lettres
+const regexNom = /^[a-zA-Z]+$/;
+
