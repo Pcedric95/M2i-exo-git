9b5a1e711802384a379032e5b19b1062c7d52202
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-17 11:29:49 +0200

    Vérifier si une heure est choisie ok

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //Sélection des éléments
 
-const form = document.querySelector('form-container');
+const form = document.querySelector('.form-container');
 const nomInput = document.querySelector('#nom');
+
 const dateInput = document.querySelector('#date');
 const heureInput = document.querySelector('#time');
+const erreurHeure = document.querySelector('#erreur-heure');
+
 const participantsInput = document.querySelector('#nbParticipants');
 const bouton = document.querySelector('button');
 
@@ -34,7 +37,14 @@ form.addEventListener('submit', (e) =>
     }
     console.log("Nom valide : ", nom)
 
+    const heure = heureInput.value;
+    erreurHeure.textContent = "";
 
+    // Vérifier si une heure est bien choisie
+    if (!heure) {
+        erreurHeure.textContent = "choisir une heure.";
+        return;
+    }
 
 })
 
