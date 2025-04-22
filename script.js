12767b09bbc56eb21fac65cffafbc8343c95e0c6
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-22 16:28:09 +0200

    étape 1 -- Ajout écoute des touches et écriture dans la grille + listes des mots possibles

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 //  Sélectionner la grille ddu html
 const grille = document.getElementById("grille");
 
+
+// La liste des mots possibles
+const motsPossibles = [
+    "POMME", "PLAGE", "TENTE", "ROUGE", "GRAIN",
+    "POINT", "VERRE", "LIVRE", "NAGER", "SAUTE"
+];
+
+// Choisir un mot au hasard
+const motMystere = motsPossibles[Math.floor(Math.random() * motsPossibles.length)];
+console.log("Le mot mystère est : " + motMystere);
+
+
 // Générer 6 lignes de 5 cases
 
 for (let ligne = 0; ligne < 6 ; ligne ++){ // Créer les 6 lignes
@@ -14,3 +26,40 @@ for (let ligne = 0; ligne < 6 ; ligne ++){ // Créer les 6 lignes
     }
     grille.appendChild(ligneDiv); //  Ici, ajouter chaque case dans chaque lignes
 }
+
+/* ----------------- Ecoute des touches + Ecriture dans la grille -------------------------*/
+
+let ligneActuelle = 0; // numéro de la ligne actuelle
+let caseActuelle = 0 // numéro de la case actuelle
+
+// sélectionner toutes les cases de grille
+const touteLesCases = document.querySelectorAll(".case");
+
+// listener sur le clavier
+document.addEventListener("keydown", (e) => {
+    const touches = e.key // récupérer la lettre tapée
+
+    // Vérifier i c'est une lettre valide ou non
+    if (/^[a-zA-Z]$/.test(touches)) {
+        //Vérifier si on a atteint ou pas 5 lettres dans la ligne
+        if (caseActuelle < 5 ) {
+            const positionDansGrille = ligneActuelle * 5 + caseActuelle;
+
+            // Insérer la lettre dans la case correspondante
+            touteLesCases[positionDansGrille].textContent = touches;
+
+            caseActuelle ++; // Passer à la case suivante
+        }
+    }
+
+    // Si la touche est Retour arrière
+    if ( touches === 'Backspace' && caseActuelle > 0) {
+        caseActuelle--; // On recule d'une case
+        const positionDansGrille = ligneActuelle * 5 + caseActuelle
+        touteLesCases[positionDansGrille].textContent = ""; // Effacement de la case
+    }
+
+    if ( touches === 'Enter' && caseActuelle === 5 ){
+        const positionDansGrille = ligneActuelle * 5 + caseActuelle
+    }
+})
