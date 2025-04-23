968a743afd030b9df947e666d93e94243476b10d
Pcedric95 <paulin.cedric95@gmail.com>
2025-04-23 09:55:27 +0200

    ajout commentaire + saut ligne pour clarté

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const grille = document.getElementById("grille");
 // La liste des mots possibles
 const motsPossibles = [
     "POMME", "PLAGE", "TENTE", "ROUGE", "GRAIN",
-    "POINT", "VERRE", "LIVRE", "NAGER", "SAUTE"
+    "POINT", "VERRE", "LIVRE", "NAGER", "SAUTE",
+    "AIDER", "FAIRE", "PLANS", "UNIES", "PLACE"
 ];
 
 // Choisir un mot au hasard
@@ -29,7 +30,7 @@ for (let ligne = 0; ligne < 6 ; ligne ++){ // Créer les 6 lignes
 
 /* ----------------- Ecoute des touches + Ecriture dans la grille -------------------------*/
 
-let ligneActuelle = 0; // numéro de la ligne actuelle
+let ligneActuelle = 0; // numéro de la ligne actuelle de l'entrée utilisateur
 let caseActuelle = 0; // numéro de la case actuelle
 
 // sélectionner toutes les cases de grille
@@ -39,7 +40,7 @@ const toutesLesCases = document.querySelectorAll(".case");
 document.addEventListener("keydown", (e) => {
     const touches = e.key // récupérer la lettre tapée
 
-    // Vérifier i c'est une lettre valide ou non
+    // Vérifier si c'est une lettre valide ou non
     if (/^[a-zA-Z]$/.test(touches)) {
         //Vérifier si on a atteint ou pas 5 lettres dans la ligne
         if (caseActuelle < 5 ) {
@@ -60,7 +61,7 @@ document.addEventListener("keydown", (e) => {
     }
 
 
-     // Si l'utilisateur appuie sur Entrée
+     // Si l'utilisateur appuie sur Entrée, validation
 
     if (touches === 'Enter' && caseActuelle === 5) {
         // Récupérer le mot tapé sur la ligne en cours
@@ -83,23 +84,27 @@ document.addEventListener("keydown", (e) => {
                 // Lettre bien placée → vert
                 caseGrille.style.backgroundColor = "green";
                 caseGrille.style.color = "white";
+           
             } else if (motMystere.includes(lettre)) {
                 // Lettre présente mais mal placée → jaune
                 caseGrille.style.backgroundColor = "gold";
                 caseGrille.style.color = "black";
+           
             } else {
                 // Lettre absente → gris
                 caseGrille.style.backgroundColor = "#aaa";
                 caseGrille.style.color = "white";
             }
         }
-     // Vérifier si le mot est correct
+    
+        // Vérifier si le mot est correct > Victoire ou Perdu
         if (motTape.toUpperCase() === motMystere) {
             document.getElementById("message").textContent = " Bravo ! Vous avez trouvé le mot !";
+       
         } else {
             ligneActuelle++;
             caseActuelle = 0;
-
+            
             // Si on a dépassé la 6e ligne
             if (ligneActuelle === 6) {
                 document.getElementById("message").textContent = ` Perdu ! Le mot mystère était "${motMystere}".`;
