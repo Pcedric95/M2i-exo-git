a0faf308fb6c48a8033d71c8cacb1c098f0d6ad1
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-05-28 15:45:11 +0200

    Ajout du 2e GET : Liste des évènements publics uniquement

--- src/Controller/ApiEventController.php
+++ src/Controller/ApiEventController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ApiEventController extends AbstractController
 {
+    // 1er GET évènements avec filtre par lieu
     #[Route('/api/events', name: 'api_events', methods: ['GET'])]
     public function index(Request $request): Response
     {
@@ -31,7 +32,7 @@ final class ApiEventController extends AbstractController
             ]
         ];
 
-        // Filtrage si "location" est présent dans l'URL
+        // Filtre apr lieu
         if ($lieu) {
             $evenements = array_filter($evenements, function ($event) use ($lieu) {
                 return strtolower($event['location']) === strtolower($lieu);
@@ -41,6 +42,35 @@ final class ApiEventController extends AbstractController
         return $this->json(array_values($evenements));
     }
 
+    // 2e GET liste évènements publiques
+    #[Route('/api/events/public', name: 'api_events_public', methods: ['GET'])]
+    public function publicEvents(): Response
+    {
+        $evenements = [
+            [
+                'id' => 1,
+                'title' => 'Conférence Symfony',
+                'location' => 'Lyon',
+                'date' => '2025-06-10',
+                'isPublic' => true,
+            ],
+            [
+                'id' => 2,
+                'title' => 'Atelier PHP Avancé',
+                'location' => 'Paris',
+                'date' => '2025-06-15',
+                'isPublic' => false,
+            ]
+        ];
+
+        $evenementsPublics = array_filter($evenements, function ($event) {
+            return $event['isPublic'] === true;
+        });
+
+        return $this->json(array_values($evenementsPublics));
+    }
+
+// 3e GET Détails d'un évènement
 
 
 
