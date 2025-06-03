b551b2c0e267a0e70d89c7db0ea07dc9688426b7
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-06-03 11:08:53 +0200

    Création lien Voir fonctionnel et page sho.html.twig opérationnelle

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'post_index')]
+    #[Route('/posts', name: 'post_index')]
     public function index(): Response
     {
         // ⚙️ Liste simulée d'articles
@@ -32,4 +32,31 @@ class PostController extends AbstractController
             'posts' => $posts,
         ]);
     }
+    #[Route('/post/{id}', name: 'post_show')]
+    public function show(int $id): Response
+    {
+        $posts = [
+            1 => [
+                'id' => 1,
+                'title' => 'Premier article',
+                'content' => 'Contenu du premier article',
+                'createdAt' => new \DateTimeImmutable('-2 days'),
+            ],
+            2 => [
+                'id' => 2,
+                'title' => 'Deuxième article',
+                'content' => 'Contenu du deuxième article',
+                'createdAt' => new \DateTimeImmutable('-1 day'),
+            ],
+        ];
+
+        if (!isset($posts[$id])) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+
+        return $this->render('post/show.html.twig', [
+            'post' => $posts[$id],
+        ]);
+    }
+
 }
