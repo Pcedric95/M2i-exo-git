a72ee4a4484461337c2623f5d2700ff1297fbc48
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-07-22 16:36:22 +0200

    Spring - Exo 04 : Gestionnaire de recettes -> CRUD complet

--- 17-JAVA/05-Spring/exo-04-Gestionnaire_recette/src/main/java/org/example/exo04gestionnaire_recette/controller/RecetteController.java
+++ 17-JAVA/05-Spring/exo-04-Gestionnaire_recette/src/main/java/org/example/exo04gestionnaire_recette/controller/RecetteController.java
@@ -1,4 +1,68 @@
 package org.example.exo04gestionnaire_recette.controller;
 
+import org.example.exo04gestionnaire_recette.model.Recette;
+import org.example.exo04gestionnaire_recette.model.Categorie;
+import org.example.exo04gestionnaire_recette.service.RecetteService;
+import org.example.exo04gestionnaire_recette.service.CategorieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/recettes")
 public class RecetteController {
+
+    @Autowired
+    private RecetteService recetteService;
+
+    @Autowired
+    private CategorieService categorieService;
+
+    // Affiche liste des recettes
+    @GetMapping
+    public String listRecettes(Model model) {
+        model.addAttribute("recettes", recetteService.getAll());
+        return "recettes/list";
+    }
+
+    // Affichage formulaire d'ajout
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("recette", new Recette());
+        model.addAttribute("categories", categorieService.getAll());
+        return "recettes/add";
+    }
+
+    // traitement formulaire d'ajout en post
+    @PostMapping("/add")
+    public String add(@Validated Recette recette) {
+        recetteService.add(recette);
+        return "redirect:/recettes";
+    }
+
+    //  affichage formulaire de modifs
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable int id, Model model) {
+        recetteService.findById(id).ifPresent(r -> {
+            model.addAttribute("recette", r);
+            model.addAttribute("categories", categorieService.getAll());
+        });
+        return "recettes/edit";
+    }
+
+    // traitement modifications
+    @PostMapping("/edit")
+    public String edit(@ModelAttribute @Validated Recette recette) {
+        recetteService.update(recette);
+        return "redirect:/recettes";
+    }
+
+    // Suppression
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        recetteService.delete(id);
+        return "redirect:/recettes";
+    }
 }
