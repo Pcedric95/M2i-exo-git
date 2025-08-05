d50aead8e5eff494a8dc2447323063b794e81a52
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-08-05 11:15:12 +0200

    ETF : Ajout ObservationrepositoryPaginate - Ajout commentaire

--- 19-ECF_05_aout_2025/Environement/src/main/java/org/example/environement/repository/ObservationRepositoryPaginate.java
+++ 19-ECF_05_aout_2025/Environement/src/main/java/org/example/environement/repository/ObservationRepositoryPaginate.java
@@ -3,5 +3,8 @@ package org.example.environement.repository;
 import org.example.environement.entity.Observation;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+
+
+// Je me suis basé sur les autres codes ici, ne sachant pas exactement à quoi cela servira
 public interface ObservationRepositoryPaginate extends PagingAndSortingRepository<Observation,Long> {
 }
