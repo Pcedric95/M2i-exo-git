af271a0351a6aaba8a5010d004c19e8dd9238c63
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-08-05 12:00:47 +0200

    ETF : TravelService -> finalisation de la classe

--- 19-ECF_05_aout_2025/Environement/src/main/java/org/example/environement/repository/TravellogRepository.java
+++ 19-ECF_05_aout_2025/Environement/src/main/java/org/example/environement/repository/TravellogRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
--- 19-ECF_05_aout_2025/Environement/src/main/java/org/example/environement/service/TravellogsService.java
+++ 19-ECF_05_aout_2025/Environement/src/main/java/org/example/environement/service/TravellogsService.java
@@ -3,6 +3,8 @@ package org.example.environement.service;
 
 import org.example.environement.dto.travellogs.TravellogDtoResponse;
 import org.example.environement.dto.travellogs.TravellogDtoStat;
+import org.example.environement.entity.Travellog;
+import org.example.environement.repository.ObservationRepository;
 import org.example.environement.repository.TravellogRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +14,28 @@ import java.util.List;
 public class TravellogsService {
 
     private final TravellogRepository travellogRepository;
+    private final ObservationRepository ObservationRepository ;
 
 
     public TravellogsService(TravellogRepository travellogRepository) {
         this.travellogRepository = travellogRepository;
+        this.ObservationRepository = ObservationRepository;
     }
 
 
     public TravellogDtoStat getStat (long id){
-        return ;
+        List<Travellog> travellogs = travellogRepository.findTravellogByObservation_Id(id);
+        TravellogDtoStat travellogDtoStat = new TravellogDtoStat();
+        return travellogDtoStat;
     }
 
     public List<TravellogDtoResponse> get(int i) {
+        return travellogRepository.findAll();
     }
 
-    public List<TravellogDtoStat> getStatForUserLastMonth (String user){
 
+    // Je sais qu'il faut retourner quelque chose mais je n'arrive pas à déterminer quoi ici
+    public List<TravellogDtoStat> getStatForUserLastMonth (String user){;
+        
     }
 }
