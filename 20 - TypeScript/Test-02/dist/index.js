b218fa43e7b6ed0f787aa71343d0087cf3ca11b1
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-08-25 16:26:21 +0200

    TypeScript : exo-02 -> consigne 02, Utilisation de NPX

---
+++ 20 - TypeScript/Test-02/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(name) {
+    return `Hello, ${name}!`;
+}
+console.log(hello("TypeScript"));
+//# sourceMappingURL=index.js.map
