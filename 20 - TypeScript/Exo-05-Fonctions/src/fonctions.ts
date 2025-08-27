4a610d20b3046acc1440e0d9cb65cf2187732f2b
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-08-27 09:56:05 +0200

    Exo - 05 -> 01 : terminé + Ajout des tests

--- 20 - TypeScript/Exo-05-Fonctions/src/fonctions.ts
+++ 20 - TypeScript/Exo-05-Fonctions/src/fonctions.ts
@@ -1,4 +1,4 @@
-// Exercice 1 :
+// Exercice 1 : Paramètres typés (budget quotidien)
 
 
 // 1 : Addition des dépenses
@@ -6,12 +6,13 @@ function additionDepenses(a: number, b: number): number {
     return a + b;
 }
 
+
 // 2 : Conversion en kilomètres
 function toKilometers(v: number | string): string {
     if (typeof v === "number") {
         return v + " km";
     }else {
-        return v + " km";
+        return v;
     }
 }
 
@@ -24,4 +25,33 @@ function totalPanier (...prix: number[]): number {
     return total;
 }
 
+// --- Tests ---
 
+console.log("## Tests pour l'Exercice 1");
+
+// Test de additionDepenses
+console.log("Résultat de l'addition :", additionDepenses(2.50, 15));
+// L'appel additionDepenses(2, "3") ne peut pas être testé ici car il est bloqué avant la compilation.
+
+console.log("---");
+
+// Tests de toKilometers
+console.log("Conversion (nombre) :", toKilometers(42));
+console.log("Conversion (chaîne) :", toKilometers("12km"));
+
+console.log("---");
+
+// Tests de totalPanier
+console.log("Total du panier (3 articles) :", totalPanier(10, 4.50, 20))
+console.log("Total du panier (0 article) :", totalPanier());
+
+
+console.log("---   Exercice 2   ---");
+
+
+
+// Exercice 2: Valeurs par défaut et optionnels (messagerie et calcul)
+
+function saluer(nom: string, message: string = "Bienvenue"): string {
+    return `${message}, ${nom}!`;
+}
