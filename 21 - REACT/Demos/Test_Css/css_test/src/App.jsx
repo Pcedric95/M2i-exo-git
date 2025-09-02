cd7616dd519201cc47332332136a1db4a8609d8a
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-02 16:18:40 +0200

    REACT - test css in js

--- 21 - REACT/Demos/Test_Css/css_test/src/App.jsx
+++ 21 - REACT/Demos/Test_Css/css_test/src/App.jsx
@@ -1,28 +1,7 @@
 import { useState } from 'react'
-import styled from 'styled-components'
 import './App.css'
 
-const Container = styled.div`
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  text-align: center;
-`;
 
-const Logo = styled.img`
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-
-  &:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  &.react:hover {
-    filter: drop-shadow(0 0 2em #61dafbaa);
-  }
-`;
 
 function App() {
 
