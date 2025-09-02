46742513c1a102f919b09150e7810c122c9c96c1
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-02 09:48:08 +0200

    REACT - TP 01 -> Logique entière terminé, affichage des pokemon fonctionnel, recherche fonctionnel

--- 21 - REACT/Exercices/TP-01-Mini_Pokédex/mini_pokedex/src/App.jsx
+++ 21 - REACT/Exercices/TP-01-Mini_Pokédex/mini_pokedex/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 // 0.1 - Créer un tableau de données Pokémon
@@ -15,25 +15,73 @@ const pokemons = [
 
 // 1 - Initialiser l'état de recherche
   const getInitialSearchItem = () => {
+
     // 1.1 - Regarder si valeur stockée dans le navigateur
     const storedTerm = localStorage.getItem('searchItem');
+    
     // 1.2 - Si oui, l'utiliser, sinon prendre le premier Pokémon
-    return storedTerm ? JSON.parse(storedTerm) : pokemons[0];
+    return storedTerm || 'Pikachu';
   }
 
 function App() {
 
   // 2 - Utiliser l'état pour le créer
-  const [searchTerm, setSearchTerm] = useState(getInitialSearchItem());
+  const [searchTerm, setSearchTerm] = useState(getInitialSearchItem);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  // 3 - Utiliser un effet pour stocker la valeur dans le navigateur
+  useEffect(() => {localStorage.setItem('searchTerm', searchTerm);}, [searchTerm]);
+
+
+  // 4 - Filtrer les Pokémon en fonction du terme de recherche
+  const filteredPokemons = pokemons.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const List = ({list}) => {
+    return (
+      <ul>
+        {list.map((item) => (
+          <li key={item.name}>
+            {item.name} ({item.type})
+          </li>
+          
+        ))}
+      </ul>
+    );
+  };
 
 
   return (
     <>
       <h1>Mini Pokédex</h1>
-      {/* Composant recherche */}
+
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+    
       {/* Composant de liste */}
+      <List list={filteredPokemons} />
+      {filteredPokemons.length === 0 && <p>Aucun Pokémon trouvé.</p>}
     </>
   )
+}
+
+
+const Search = ({ searchTerm, onSearch }) => {
+  return (
+    <div>
+      <label htmlFor="search">Rechercher un Pokémon :</label>
+      <input
+        id='search'
+        type="text"
+        placeholder="Rechercher un Pokémon..."
+        value={searchTerm}
+        onChange={onSearch}
+      />
+    </div>
+  );
 }
 
 export default App
