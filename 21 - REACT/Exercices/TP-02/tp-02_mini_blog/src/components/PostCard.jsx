6d84e1a169592d191a041f88e7bfdedbb98a0980
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-03 14:34:49 +0200

    REACT - TP-02 -> Création du lien dans PostCard avec <Link> + Création de la page PostDetailPage

--- 21 - REACT/Exercices/TP-02/tp-02_mini_blog/src/components/PostCard.jsx
+++ 21 - REACT/Exercices/TP-02/tp-02_mini_blog/src/components/PostCard.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
 
 
 
 const PostCard = ({post }) => {
     return (
         <div style={{border: '1px solid #ccc', padding: '20px', margin:'15px 0'}}>
-            <h3>{post.title}</h3>
+            <Link to={`/post/*{post.id}`}>
+                <h3>{post.title}</h3>
+            </Link>
             <p>{post.body}</p>
         </div>
     )
---
+++ 21 - REACT/Exercices/TP-02/tp-02_mini_blog/src/pages/PostDetailPage.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getPostById} from '../services/api'
+
+
+const PostDetailPage = () => {
+    const [post, setPost] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const { id } = useParams(); // Récupérer ID URL
+
+    useEffect(() => {
+      const fetchPost = async () => {
+        try {
+          const postData = await getPostById(id);
+          setPost(postData);
+          setLoading(false);
+        } catch (error) {
+          console.error(
+            "Erreur lors de la récupération de l'article : ",
+            error
+          );
+        }
+      };
+      fetchPost();
+    }, [id]);
+
+    if (loading) {
+        return <div>Chargement de l'article...</div>;
+    }
+
+    if (!post) {
+        return <div>Article non trouvé</div>;
+    }
+
+
+    return (
+        <div>
+            <h2> {post.title} </h2>
+        </div>
+    );
+}
+
+export default PostDetailPage
