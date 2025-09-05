8bfd7a8a903ffdf6f025e33e5d6f13f8f1503047
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-05 16:40:13 +0200

    REACT - TP-03 : Mini-Pokédex Niveau 2 -> 1.2 : Ajouter des couleurs par types de pokémon

--- 21 - REACT/Exercices/TP-03/src/components/PokemonCard/PokemonCard.jsx
+++ 21 - REACT/Exercices/TP-03/src/components/PokemonCard/PokemonCard.jsx
@@ -5,8 +5,20 @@ const PokemonCard = ({ name, type }) => {
   // URL pour les images des Pokémon (exemple avec PokeAPI ou images locales)
   const imageUrl = `https://img.pokemondb.net/artwork/large/${name.toLowerCase()}.jpg`;
 
+  // Type par couleur
+  const typeColors = {
+  Electrik: '#FFD700', // Jaune
+  Plante: '#7CFC00',   // Vert
+  Feu: '#FF4500',      // Orange
+  Eau: '#1E90FF',      // Bleu
+  Normal: '#A9A9A9',   // Gris
+  Roche: '#A0522D',     // Marron
+  Combat: '#8B0000',    // Rouge foncé
+};
+
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} style={{ backgroundColor: typeColors[type] || '#FFFFFF' }}>
       <img src={imageUrl} alt={name} className={styles.image} onError={(e) => { e.target.onerror = null; e.target.src = "https://via.placeholder.com/150?text=Pokémon"; }} />
       <h3>{name}</h3>
       <p className={styles.type}>Type: {type}</p>
