d0790161326cdbb59f4ab30e3e0139d8a7c54445
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-08 14:06:06 +0200

    React Native : Test d'ajout de style

--- 22 - React Native/Test-termplate-App/App.js
+++ 22 - React Native/Test-termplate-App/App.js
@@ -1,30 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
-
-if (Platform === 'android') {
+// Affichage si android ou ios
+/* if (Platform === 'android') {
   console.log("On tourne sur un Android !");
 } else if (Platform === 'ios') {
   console.log("On tourne sur un iOS !");
-}
+} */
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <Text>Ceci est un deuxième test sur React Native</Text>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <Text style={styles.title}> Mon premier titre</Text>
+          <Text style={styles.content}>Contenu de mon app</Text>
 
-      <View style={styles.container}>
-        {/* Contenu d'un header par exemple */}
-        <Text >Header</Text>
-      </View>
-
-      <View style={styles.container}>
-        {/* Contenu d'un footer par exemple */}
-        <Text>Footer</Text>
-      </View>
+        </View>
+      </SafeAreaView>
     </>
   );
 }
@@ -33,7 +26,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 20,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  safeArea:{
+    flex: 1,
+    // Fix pour android
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: '#007AFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#cd0b0b81',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  content: {
+    fontFamily: "Arial",
   },
 });
