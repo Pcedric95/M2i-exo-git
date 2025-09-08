4fcc5536988ec803132e112e32bfb0a31539a296
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-08 16:55:02 +0200

    React Native - Exo-02 -> Repartir de la base Exo-01

---
+++ 22 - React Native/Exercices/Exo-01_User_Card_Part_2/components/UserCard.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Alert, Animated } from 'react-native';
+
+const UserCard = ({ name, job, description, email, phone, avatar }) => {
+    return (
+        <View style={styles.card}>
+            {/* Avatar */}
+            <Image 
+            source={{ uri: avatar }}
+            style={styles.avatar}
+            />
+            {/* Informations utilisateur */} */}
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.job}>{job}</Text>
+            <Text style={styles.description}> {description} </Text>
+
+            {/* Contact */}
+            <View style={styles.contact}>
+                <Text style={styles.contactLabel}>Email :</Text>
+                <Text style={styles.contactText}>{email}</Text>
+                <Text style={styles.contactLabel}>Téléphone:</Text>
+                <Text style={styles.contactText}>{phone}</Text>
+            </View>
+
+            <TouchableOpacity 
+            style={styles.button}
+            onPress={() => Alert.alert('Contact', 'Vous voulez contacter ${name} ')}
+            >
+                <Text style={styles.buttonText}>Contacter</Text>
+            </TouchableOpacity>
+
+            
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    margin: 15,
+    elevation: 3,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: "center",
+    marginBottom: 16,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  job: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 16,
+  },
+  contact: {
+    marginTop: 10,
+  },
+  contactLabel: {
+    fontWeight: "bold",
+  },
+  contactText: {
+    marginBottom: 8,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignSelf: "center",
+    marginTop: 16,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+export default UserCard;
