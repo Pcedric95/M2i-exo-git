89089c1c8d38c3adebb932d632fcc6ef415e52ba
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-15 16:09:55 +0200

    Kotlin - Exercice 01 : Nom complet

---
+++ 23 - Kotlin/Exercices/Exercices-Base-Kotlin/src/Kotlin/01_Nom_Complet.kt
@@ -0,0 +1,14 @@
+// 01 - Nom complet
+
+fun nomComplet(prenom: String, nom: String) = "$prenom $nom"
+
+fun main() {
+    print("Entrez votre prénom : ")
+    val prenom = readLine() ?: ""
+
+    print("Entrez votre nom : ")
+    val nom = readLine() ?: ""
+
+    val resultat = nomComplet(prenom, nom)
+    println("Votre nom complet est : $resultat")
+}
