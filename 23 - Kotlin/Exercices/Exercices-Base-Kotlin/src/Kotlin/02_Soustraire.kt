24c0557b7b07af12f3356e641790027f4f78c9f3
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-15 16:12:38 +0200

    Kotlin - Exercice 02 : Soustraction

---
+++ 23 - Kotlin/Exercices/Exercices-Base-Kotlin/src/Kotlin/02_Soustraire.kt
@@ -0,0 +1,9 @@
+fun soustraire(a: Int, b: Int): Int {
+    println("Je soustrais $a et $b")
+    return a - b
+}
+
+fun main() {
+    val resultatSoustraction = soustraire(2, 1)
+    println("Résultat : $resultatSoustraction")
+}
