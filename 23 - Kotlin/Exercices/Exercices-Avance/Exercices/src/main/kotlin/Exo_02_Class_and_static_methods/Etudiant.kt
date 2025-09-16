7877ef8b1c7dfe785ec2b4bbb3f34b304ad2cfe6
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-16 12:28:39 +0200

    Kotlin Avancé - Exo 02 -> Etudiants - Classe avec propriété et méthode statique.

---
+++ 23 - Kotlin/Exercices/Exercices-Avance/Exercices/src/main/kotlin/Exo_02_Class_and_static_methods/Etudiant.kt
@@ -0,0 +1,38 @@
+package Exo_02_Class_and_static_methods
+
+class Etudiant (val nom: String, val age: Int){
+
+
+    // pour chaque étudiant créé, le compteur augmente de 1
+    init {
+        compteur++
+    }
+
+    companion object {
+        // Variable statique compteur
+        private var compteur: Int = 0
+
+        // La méthode statique affiche le nombre total d'etudiant crée
+        fun afficherCompteur() {
+            print("Nombres total d'étuditans crée : $compteur")
+        }
+    }
+    fun description(){ println("L'etudiant $nom a $age ans") }
+    fun détailsEtudiant() { println("Nom : $nom, Age : $age") }
+}
+
+fun main() {
+    val etudiant_01 = Etudiant("Paul", 25)
+    val etudiant_02 = Etudiant("Pierre", 22)
+    val etudiant_03 = Etudiant("Jeanne", 21)
+    val etudiant_04 = Etudiant("Marie", 23)
+
+
+    Etudiant.afficherCompteur()
+
+    etudiant_01.description()
+    etudiant_02.détailsEtudiant()
+    etudiant_03.description()
+    etudiant_04.détailsEtudiant()
+
+}
