9a22bfb59926e2e12daa4cf6d290b82b90be30ac
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-22 22:09:31 +0200

    Android studio - TP02 -> Correction des problèmes d'ouverture et de fermeture balise

--- 24-Android_Studio/Exercices/TP02Gestionnaire_note_frais/app/src/main/java/com/example/tp_02_gestionnaire_note_frais/MainActivity.kt
+++ 24-Android_Studio/Exercices/TP02Gestionnaire_note_frais/app/src/main/java/com/example/tp_02_gestionnaire_note_frais/MainActivity.kt
@@ -6,6 +6,42 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+data class NoteFrais(
+    var nomEmploye: String = "",
+    var numeroEmploye: String = "",
+    var departement: String = "",
+    var typeFrais: String = "",
+    var montant: Double = 10.0,
+    var avecTVA: Boolean = false,
+    var fraisRecurrent: Boolean = false,
+    var justificatifFourni: Boolean = false,
+    var urgence: String = "Normal"
+) {
+    fun isNomValide(): Boolean {
+        val mots = nomEmploye.trim().split("\\s+".toRegex())
+        return mots.size >= 2 && mots.all { it.isNotEmpty() }
+    }
+
+    fun isNumeroValide(): Boolean {
+        return numeroEmploye.matches("\\d{5}".toRegex())
+    }
+
+    fun calculerMontantTTC(): Double {
+        return if (avecTVA) montant * 1.20 else montant
+    }
+}
+
+/*private lateinit var noteFrais: NoteFrais
+private lateinit var montantTextView: TextView
+private lateinit var montantSeekBar: SeekBar
+private lateinit var ticketTextView: TextView
+private lateinit var budgetProgressBar: ProgressBar*/
+
+
+
+
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
