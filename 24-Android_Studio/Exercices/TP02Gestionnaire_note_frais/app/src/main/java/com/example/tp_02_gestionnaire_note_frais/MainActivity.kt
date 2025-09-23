b988a69d7ac523cc1d7ae51f0766a42a42a99a65
Pcedric95 <167176176+Pcedric95@users.noreply.github.com>
2025-09-23 09:27:12 +0200

    TP02 -> Ajout des fonctions logique métiers (en cours)

--- 24-Android_Studio/Exercices/TP02Gestionnaire_note_frais/app/src/main/java/com/example/tp_02_gestionnaire_note_frais/MainActivity.kt
+++ 24-Android_Studio/Exercices/TP02Gestionnaire_note_frais/app/src/main/java/com/example/tp_02_gestionnaire_note_frais/MainActivity.kt
@@ -31,26 +31,33 @@ data class NoteFrais(
     }
 }
 
-/*private lateinit var noteFrais: NoteFrais
-private lateinit var montantTextView: TextView
-private lateinit var montantSeekBar: SeekBar
-private lateinit var ticketTextView: TextView
-private lateinit var budgetProgressBar: ProgressBar*/
-
-
-
-
-
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteFrais: NoteFrais
+    private lateinit var montantTextView: TextView
+    private lateinit var montantSeekBar: SeekBar
+    private lateinit var montantTTCTextView: montantTTCTextView
+    private lateinit var budgetProgressBar: ProgressBar
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        noteFrais = NoteFrais()
+        initialiserComposants()
+        configurerEcouteurs()
     }
+
+    private fun initialiserComposants() {
+        montantTextView = findViewById(R.id.montantTextView)
+        montantSeekBar = findViewById(R.id.montantSeekBar)
+        montantTTCTextView = findViewById(R.id.montantTTCTextView)
+        budgetProgressBar = findViewById(R.id.budgetProgressBar)
+    }
+
+    private fun configurerEcouteurs() {}
+
 }
