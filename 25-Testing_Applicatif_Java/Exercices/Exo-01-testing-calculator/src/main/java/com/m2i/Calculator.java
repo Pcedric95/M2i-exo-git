ebf9cd82d1ef867c5d48da563bcc83c297fb0358
Paulin.cedric <167176176+Pcedric95@users.noreply.github.com>
2025-09-25 11:12:46 +0200

    Testing Applicatif - Exo 01 - Initialisation du projet et classe calculator

---
+++ 25-Testing_Applicatif_Java/Exercices/Exo-01-testing-calculator/src/main/java/com/m2i/Calculator.java
@@ -0,0 +1,19 @@
+package com.m2i;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) throws ArithmeticException {
+        return a / b;
+    }
+}
