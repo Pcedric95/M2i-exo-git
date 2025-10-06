cbdb3dfce159ce8cdca229d47659f1220f2fd320
Paulin.cedric <167176176+Pcedric95@users.noreply.github.com>
2025-10-06 10:38:09 +0200

    1 - Création des deux composants manquants : EmployeeForm  + EmployeeList + style EmployeeList

---
+++ 27 - ECF_React/26-ECF_06_OCTOBRE_2025/frontend/src/components/EmployeeForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmployeeForm = () => {
+  return (
+    <div>
+      <h2>Formulaire Employé</h2>
+      <p>Composant en cours de développement...</p>
+    </div>
+  );
+};
+
+export default EmployeeForm;
