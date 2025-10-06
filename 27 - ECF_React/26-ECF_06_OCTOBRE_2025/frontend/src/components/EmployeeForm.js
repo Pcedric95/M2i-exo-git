e1a46fc9481ffa9d3992aba4191219b5210adc28
Paulin.cedric <167176176+Pcedric95@users.noreply.github.com>
2025-10-06 11:41:28 +0200

    2.1 - Composant EmployeeForm -> style terminé (non ressemblant à 100 %) + suppression d'une ligne inutile

--- 27 - ECF_React/26-ECF_06_OCTOBRE_2025/frontend/src/components/EmployeeForm.js
+++ 27 - ECF_React/26-ECF_06_OCTOBRE_2025/frontend/src/components/EmployeeForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { use } from 'react';
 import {useParams, useNavigate} from 'react-router-dom';
+import './EmployeeForm.css';
 
 const EmployeeForm = () => {
 
